eb1754878b790f5d8233220264e3daccac248879
SAMtechBME <165875112+SAMtechBME@users.noreply.github.com>
2025-04-28 11:36:58 -0700

    config.py
    updated it for yaml object

--- train_code/config.py
+++ train_code/config.py
@@ -4,7 +4,16 @@ import yaml
 class Config:
     def __init__(self, yaml_file):
         with open(yaml_file, 'r') as f:
-            self.opt = yaml.safe_load(f)
+            cfg_dict = yaml.safe_load(f)
+        
+        for k, v in cfg_dict.items():
+            setattr(self, k, DictToObject(v))
 
-    def __getattr__(self, name):
-        return self.opt.get(name)
+class DictToObject:
+    def __init__(self, dict_):
+        for k, v in dict_.items():
+            if isinstance(v, dict):
+                setattr(self, k, DictToObject(v))
+            else:
+                setattr(self, k, v)
+
