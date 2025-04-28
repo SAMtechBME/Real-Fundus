8de7b2156038dd4c9e695dd275a415686e6d6640
SAMtechBME <165875112+SAMtechBME@users.noreply.github.com>
2025-04-28 06:19:33 -0700

    train.py
    updated the third line

--- train_code/train.py
+++ train_code/train.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append('/content/Real-Fundus')
 
 from config import Config 
 opt = Config('train.yml')
